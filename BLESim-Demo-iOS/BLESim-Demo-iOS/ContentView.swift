801c1ccf90db441048431b766f83fb4a28105939
saqibomer <saqibomer.cs@gmail.com>
2025-09-24 13:57:53 +0500

    Added iOS demo

---
+++ BLESim-Demo-iOS/BLESim-Demo-iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BLESim-Demo-iOS
+//
+//  Created by Saqib Omer on 24/09/2025.
+//  GitHub: https://github.com/saqibomer
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
